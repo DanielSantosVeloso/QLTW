71e6487be24b496811ebf97577eea19ab7c1f913
GustavoVieiraSousa <gustavovieirasousa1v1@gmail.com>
2024-04-27 20:19:28 -0300

    a merda vindo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("com.google.gms.google-services")
+
+
 }
 
 android {
@@ -39,13 +41,19 @@ android {
 
 dependencies {
 
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-analytics")
+
 }
--- app/src/main/java/com/example/qltw/CadastroActivity.kt
+++ app/src/main/java/com/example/qltw/CadastroActivity.kt
@@ -3,17 +3,30 @@ package com.example.qltw
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class CadastroActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -24,5 +37,53 @@ class CadastroActivity : AppCompatActivity() {
             val  intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
-    }
-}
+
+
+                }
+    fun fazerCadastro(View : View) {
+        val email = findViewById<EditText>(R.id.Email_cad)
+        val senha = findViewById<EditText>(R.id.Senha_cad)
+
+        if(email.text.isEmpty() || senha.text.isEmpty()){
+            Toast.makeText(this, "Por favor preencha os campos", Toast.LENGTH_SHORT)
+                .show()
+            return
+
+        }
+        val  inputEmail = email.toString()
+        val inputPassword = senha.toString()
+
+        auth.createUserWithEmailAndPassword(inputEmail,inputPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(
+                        baseContext,
+                        "Registro complesto",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                }
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Um erro ocorreu ${it.localizedMessage}", Toast.LENGTH_SHORT)
+                    .show()
+
+        }}}
+
+
+
+
+
