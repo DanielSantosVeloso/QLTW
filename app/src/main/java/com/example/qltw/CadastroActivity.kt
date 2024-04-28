96674a7dc8507e038224f6d3e985892fbd848498
GustavoVieiraSousa <gustavovieirasousa1v1@gmail.com>
2024-04-27 23:10:06 -0300

    popo

--- app/src/main/java/com/example/qltw/CadastroActivity.kt
+++ app/src/main/java/com/example/qltw/CadastroActivity.kt
@@ -21,19 +21,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 import com.google.firebase.auth.ktx.auth
 
 
 class CadastroActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
+
+    private lateinit var sqlHelper: sqlHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Initialize Firebase Auth
-        auth = Firebase.auth
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro)
+
+        sqlHelper = sqlHelper(this)
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -44,9 +49,31 @@ class CadastroActivity : AppCompatActivity() {
         registrarTextos.setOnClickListener{
             val  intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-        }
 
+
+        }
 
                 }
 
+    private fun cadastroBase() {
+        val NomeUsuario: EditText = findViewById(R.id.Nomecad)
+        val passwordUsuario: EditText = findViewById(R.id.Senha_cad)
+        val emailUsuario: EditText = findViewById(R.id.Email_cad)
+        val username = NomeUsuario.text.toString()
+        val password = passwordUsuario.text.toString()
+        val email = emailUsuario.text.toString()
+
+
+        val colunaPeloID = sqlHelper.inserirUsuario(username, password, email)
+        if(colunaPeloID != -1L){
+            Toast.makeText(this,"Cadastro feito com sucesso", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            Toast.makeText(this,"Erro no cadastro", Toast.LENGTH_SHORT).show()
         }
+
+    }
+
+}
